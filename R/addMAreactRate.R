0f1da25119590f7131313b7798fcdfff60794d97
Brady <brady.johnston@me.com>
2020-12-15 19:45:29 +1100

    change reactrate and addspecies

--- R/addMAreactRate.R
+++ R/addMAreactRate.R
@@ -1,74 +1,76 @@
 #' Adding reaction rates
-#' 
-#' This function allows users to define reaction rate. Reaction rate can be 
-#' defined as a fixed numerical value (e.g., r=m1, where m1 represents a 
-#' constant parameter, m1=0.9, or just as r=0.9) or initial assignments 
-#' (e.g., r=m1*A, where m1 represents a constant parameter and A represents 
+#'
+#' This function allows users to define reaction rate. Reaction rate can be
+#' defined as a fixed numerical value (e.g., r=m1, where m1 represents a
+#' constant parameter, m1=0.9, or just as r=0.9) or initial assignments
+#' (e.g., r=m1*A, where m1 represents a constant parameter and A represents
 #' a species).
-#' 
+#'
 #' @param x  model to which the reaction rate is added (required)
 #' @param rr reaction rate name (required),
-#' @param type reaction rate type; currently supported options are fixed and assigned; 
+#' @param type reaction rate type; currently supported options are fixed and assigned;
 #'     if the value is not specified, sysBio will set type to fixed
-#' @param val reaction rate value; if defined as fixed, the value should be specified as a number. 
-#'     e.g., 0.1; if defined as assigned, the value can be defined in form of an assignment (function), 
+#' @param val reaction rate value; if defined as fixed, the value should be specified as a number.
+#'     e.g., 0.1; if defined as assigned, the value can be defined in form of an assignment (function),
 #'     e.g., B or 10*A. If the value is not specified, sysBio will set the value to 1
 #' @param overwrite a flag that allows changes to the existing reaction rate (default value FALSE)
-#'     
-#' @return This function adds information about reaction rates into the model (given as a first argument of the function). 
+#'
+#' @return This function adds information about reaction rates into the model (given as a first argument of the function).
 #'     Reaction rates information is stored in the list format and contain the following elements:
 #'     \itemize{
 #'     \item{rates$rrName - reaction rate name}
 #'     \item{rates$rType - reaction rate type}
 #'     \item{rates$rVal - reaction rate value}
 #'     }
-#'     
+#'
 #' @examples
 #' exmp <- newModel("This is an example of a new model")
 #' addMAreaction(exmp, react="A = null", "rf", "rb")
 #' addMAreaction(exmp, react="A + B -> 2*AB", "k", name="Forward AB")
 #' addMAreaction(exmp, react="AB -> null", "rAB")
-#' 
+#'
 #' addMAreactRate(exmp, "rf", "fixed", "1")
 #' addMAreactRate(exmp, "rb", "fixed", "0.75")
 #' addMAreactRate(exmp, "k", "fixed", "0.5")
 #' addMAreactRate(exmp, "rAB", "assigned", "p1*A")
-#' 
-#'  
+#'
+#'
 #' # Show info about model reactions and rates
 #' exmp$reaction
 #' exmp$rates
-#' 
+#'
 #' @export
-#' 
+#'
 
 #addMAreactRate.function <- function(x, rr=NA, type="fixed", val="1", overwrite=FALSE){
-addMAreactRate <- function(x, rr=NA, type="fixed", val="1", overwrite=FALSE){  
-  if (!exists(deparse(substitute(x))))
+addMAreactRate <- function(x, rr=NA, type="fixed", val="1", overwrite=FALSE){
+  if ("list" %in% is(x)) {
+  } else {
     stop("Specified model does not exist!")
-  
-  if (is.na(rr))
-    stop("Reaction rate is not specified!")
-  
-  
-  if (!(type %in% c("fixed", "assigned"))) 
-    stop("Reaction type is not in the correct format!")
+  }
+
+  if (is.na(rr)) {
+    stop("Reaction rate is not specified!")}
+
+
+  if (!(type %in% c("fixed", "assigned"))) {
+    stop("Reaction type is not in the correct format!")}
   # Add option to use odes
-  
+
   y <- x
-  
+
   # Current number of species
   curNumber <- length(y$rates)
-  
+
   # Check if a reaction rate with the same name exist
   if (curNumber > 0){
     if (rr %in% y$rates$rrName){
-      
+
       print("Rate with the same name already exist in the model!")
-      
+
       if (overwrite){
         getIndex <- which(y$rates$rrName == rr)
-        
+
         if ((y$rates$rType[getIndex] != type) | (y$rates$rVal[getIndex] != val)){
           y$rates$rType[getIndex] <- type
           y$rates$rVal[getIndex] <- val
@@ -76,22 +78,23 @@ addMAreactRate <- function(x, rr=NA, type="fixed", val="1", overwrite=FALSE){
         } else {
           print("The new initial value and/or type of the reaction are the same as the old one")
         }
-        
+
       } else {
         stop("Specify a different reaction name or set overwrite flag to TRUE!")
       }
     } else {
       y$rates$rrName <- c(y$rates$rrName, rr)
       y$rates$rType <- c(y$rates$rType, type)
-      y$rates$rVal <- c(y$rates$rVal, val)   
+      y$rates$rVal <- c(y$rates$rVal, val)
     }
   } else {
-    y$rates <- list(rrName=rr, rType=type, rVal=val) 
+    y$rates <- list(rrName=rr, rType=type, rVal=val)
   }
-  
+
   y$isChecked <- 0
-  
-  assign(deparse(substitute(x)), y, envir = .GlobalEnv)  
+
+  # assign(deparse(substitute(x)), y, envir = .GlobalEnv)
+  y
 }
 
 #addMAreactRate <- cmpfun(addMAreactRate.function)
--- R/addSpecies.R
+++ R/addSpecies.R
@@ -1,90 +1,93 @@
 #' Adding species
-#' 
-#' This function allows users to specify initial values for species within the model. 
-#' Model must contain at least one species. Initial values for all species, as well as all rates and 
-#' parameters have to be defined. sysBio uses a term null to describe a null species (source or sink); 
-#' null species do not need to be defined. 
-#' 
+#'
+#' This function allows users to specify initial values for species within the model.
+#' Model must contain at least one species. Initial values for all species, as well as all rates and
+#' parameters have to be defined. sysBio uses a term null to describe a null species (source or sink);
+#' null species do not need to be defined.
+#'
 #' @param x  model to which the species is added (required)
 #' @param spName species name (required)
 #' @param iVal initial value; if the value is not specified, sysBio will set initial value to 0
 #' @param overwrite a flag that allows changes to the existing species (default value FALSE)
-#'     
-#' @return This function adds information about species into the model (given as a first argument of the function). 
+#'
+#' @return This function adds information about species into the model (given as a first argument of the function).
 #'     Species information is stored in the list format and contain the following elements:
 #'     \itemize{
 #'     \item{species$sName - species name}
 #'     \item{species$initVal - initial value}
 #'     }
-#'     
+#'
 #' @examples
 #' exmp <- newModel("This is an example of a new model")
 #' addMAreaction(exmp, react="A = null", "rf", "rb")
 #' addMAreaction(exmp, react="A + B -> 2*AB", "k", name="Forward AB")
 #' addMAreaction(exmp, react="AB -> null", "rAB")
-#' 
+#'
 #' addMAreactRate(exmp, "rf", "fixed", "1")
 #' addMAreactRate(exmp, "rb", "fixed", "0.75")
 #' addMAreactRate(exmp, "k", "fixed", "0.5")
 #' addMAreactRate(exmp, "rAB", "assigned", "p1*A")
-#' 
+#'
 #' addParameters(exmp, "p1", 0.75)
-#'  
+#'
 #' addSpecies(exmp, "A", 10)
 #' addSpecies(exmp, "B", 10)
 #' addSpecies(exmp, "AB", 0)
-#'   
+#'
 #' # Show info about model reactions and species
 #' exmp$reaction
 #' exmp$species
-#' 
+#'
 #' @export
-#' 
+#'
 
 #addSpecies.function <- function(x, spName=NA, iVal="0", overwrite=FALSE){
 addSpecies <- function(x, spName=NA, iVal="0", overwrite=FALSE){
-  
-  if (!exists(deparse(substitute(x))))
+
+  if ("list" %in% is(x)) {
+  } else {
     stop("Specified model does not exist!")
-  
-  if (is.na(spName))
-    stop("Species name is not specified!")
-  
+  }
+
+  if (is.na(spName)) {
+    stop("Species name is not specified!")}
+
   y <- x
-  
+
   # Current number of species
   curNumber <- length(y$species)
-  
+
   # Check if species with the same name exist
   if (curNumber > 0){
     if (spName %in% y$species$sName){
-      
+
       print("Species with the same name already exist in the model!")
-      
+
       if (overwrite){
         getIndex <- which(y$species$sName == spName)
-        
+
         if (y$species$initVal[getIndex] != iVal){
           y$species$initVal[getIndex] <- iVal
           print("Initial value has been replaced")
         } else {
           print("The new initial value is the same as the old one")
         }
-        
+
       } else {
         stop("Specify a different species or set overwrite flag to TRUE!")
       }
     } else {
-      y$species$sName <- c(y$species$sName, spName)    
-      y$species$initVal <- c(y$species$initVal, iVal)   
+      y$species$sName <- c(y$species$sName, spName)
+      y$species$initVal <- c(y$species$initVal, iVal)
     }
   } else {
-    y$species <- list(sName=spName, initVal=iVal)  
+    y$species <- list(sName=spName, initVal=iVal)
   }
-  
+
   y$isChecked <- 0
-  
-  assign(deparse(substitute(x)), y, envir = .GlobalEnv)  
+
+  # assign(deparse(substitute(x)), y, envir = .GlobalEnv)
+  y
 }
 
 #addSpecies <- cmpfun(addSpecies.function)
