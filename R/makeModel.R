679dd4417c51391f1cb3e6a51f3bc4f6d87b0bea
Brady <brady.johnston@me.com>
2020-12-15 19:56:15 +1100

    update makemodel and validatemodel

--- R/makeModel.R
+++ R/makeModel.R
@@ -1,18 +1,18 @@
 #' Creating a model
-#' 
-#' This function creates mathematical model of the system, a set 
-#' of differential equations that correspond to that mathematical 
-#' model, stochastic matrix, and propensity function. It also 
-#' transforms all defined rates, parameters, and rules into a 
+#'
+#' This function creates mathematical model of the system, a set
+#' of differential equations that correspond to that mathematical
+#' model, stochastic matrix, and propensity function. It also
+#' transforms all defined rates, parameters, and rules into a
 #' format required by the ODEs solver function.
-#' 
+#'
 #' @param x  model name (required)
-#'     
-#' @return This function adds updates model (given as an argument 
-#'     of the function) with information about mathematical model 
-#'     of the system, ODEs, stochastic matrix, and propensity 
-#'     function. This function also updates model's isChecked 
-#'     flag, as model is created based on the most recent data. 
+#'
+#' @return This function adds updates model (given as an argument
+#'     of the function) with information about mathematical model
+#'     of the system, ODEs, stochastic matrix, and propensity
+#'     function. This function also updates model's isChecked
+#'     flag, as model is created based on the most recent data.
 #'     It updates the following elements:
 #'     \itemize{
 #'     \item{model - mathematical model of the system}
@@ -24,131 +24,133 @@
 #'     \item{stochModel - propensity function (one equation for each reaction)}
 #'     \item{isChecked - flag set to 1}
 #'     }
-#'     
+#'
 #' @examples
 #' exmp <- newModel("This is an example of a new model")
 #' addMAreaction(exmp, react="A = null", "rf", "rb")
 #' addMAreaction(exmp, react="A + B -> 2*AB", "k", name="Forward AB")
 #' addMAreaction(exmp, react="AB -> null", "rAB")
-#' 
+#'
 #' addMAreactRate(exmp, "rf", "fixed", "1")
 #' addMAreactRate(exmp, "rb", "fixed", "0.75")
 #' addMAreactRate(exmp, "k", "fixed", "0.5")
 #' addMAreactRate(exmp, "rAB", "assigned", "p1*A")
-#' 
+#'
 #' addParameters(exmp, "p1", 0.75)
-#'  
+#'
 #' addSpecies(exmp, "A", 10)
 #' addSpecies(exmp, "B", 10)
 #' addSpecies(exmp, "AB", 0)
-#' 
+#'
 #' addRule(exmp, "rule B", "ODEs", "B=-0.1*AB")
-#' 
+#'
 #' # Show info about the model
 #' exmp
-#' 
+#'
 #' makeModel(exmp)
-#'   
+#'
 #' # Show info about the updated model
 #' exmp
-#' 
+#'
 #' @export
-#' 
+#'
 
 #makeModel.function <- function(x){
 makeModel <- function(x){
-  
-  if (!exists(deparse(substitute(x))))
+
+  if ("list" %in% is(x)) {
+  } else {
     stop("Specified model does not exist!")
-  
+  }
+
   # We'll use y to make all modifications
   y <- x
-  
+
   # Check if there's any reaction in the model
   numR <- length(y$reaction$rName)
-  
+
   if (numR > 0)
   {
     # Create temporary data frame
-    tmpDF1 <- data.frame(stoch=c(), species=c(), side=c(), reaction=c(), product=c(), odeSpecies=c(), odeProduct=c()) 
-    
+    tmpDF1 <- data.frame(stoch=c(), species=c(), side=c(), reaction=c(), product=c(), odeSpecies=c(), odeProduct=c())
+
     # Temp variable (counter) for reversible reaction case
     cnt.r2 <- 1
-    
+
     for (i in 1:numR){
-      
-      # Remove spaces from the reaction 
-      react.cln <- gsub(" ","", y$reaction$reaction[i] , fixed=TRUE)      
-      
+
+      # Remove spaces from the reaction
+      react.cln <- gsub(" ","", y$reaction$reaction[i] , fixed=TRUE)
+
       # Check if it is reversible reaction
       whichOne <- length(strsplit(react.cln, "\\->")[[1]])
-      
+
       if (whichOne == 1) #Reversible reaction
       {
         # Get both sides of reaction
         x.react <- strsplit(react.cln, "\\=")[[1]]
-        
+
         if (length(x.react) > 2)
           print("Error! Cascades are currently not supported")
-        
+
         if (length(x.react) == 1)
           print("Error! Reaction is not in a correct format! Use -> or = symbols!")
-        
+
         # Parse both directions of reaction
         tmpDF1 <- rbind(tmpDF1, parseReaction(x, paste(x.react[1], x.react[2], sep="->"), y$reaction$r1[i], paste(i, "1", sep="_")))
         tmpDF1 <- rbind(tmpDF1, parseReaction(x, paste(x.react[2], x.react[1], sep="->"), y$reaction$r2[cnt.r2], paste(i, "2", sep="_")))
         cnt.r2 <- cnt.r2 + 1
-        
-      } else 
+
+      } else
         if (whichOne == 2)
           {
             # Parse reaction (only one direction)
-            tmpDF1 <- rbind(tmpDF1, parseReaction(x, react.cln, y$reaction$r1[i],as.character(i)))        
+            tmpDF1 <- rbind(tmpDF1, parseReaction(x, react.cln, y$reaction$r1[i],as.character(i)))
         } else {
         print("Error! Cascades are currently not supported")
       }
     }
-    
+
     # Remove any species that is called "null" (sources or sinks)
     tmpDF2 <- tmpDF1[as.character(tmpDF1$species) != "null", ]
-    
+
     y$stochMatrix <- makeStMat(x, tmpDF2)
     y$stochModel <- makeStModel(x, tmpDF2)
-    
+
     # Summarize per species quantity (per reaction and product)
-    tmpDF1 <- plyr::ddply(tmpDF2, .variable=c("species", "reaction", "product", "odeSpecies", "odeProduct"), function(x) data.frame(tot=sum(as.numeric(x$stoch)*as.numeric(x$side))))     
-    
+    tmpDF1 <- plyr::ddply(tmpDF2, .variable=c("species", "reaction", "product", "odeSpecies", "odeProduct"), function(x) data.frame(tot=sum(as.numeric(x$stoch)*as.numeric(x$side))))
+
     # Remove those with total equal to zero (that canceled out)
     tmpDF2 <- tmpDF1[as.numeric(tmpDF1$tot) != 0, ]
-    
+
     # Summarize per species and reactions (in the form reaction times product)
     tmpDF1 <- plyr::ddply(tmpDF2, .variable=c("species", "reaction", "odeSpecies"), function(x) data.frame(t1=ifelse(x$tot == 1, paste(as.character(x$reaction), x$product, sep="*"), ifelse(x$tot == -1, paste("-", paste(as.character(x$reaction),x$product, sep="*"), sep=""), paste(as.character(x$tot), as.character(x$reaction), x$product, sep="*"))), t2=ifelse(x$tot == 1, paste(as.character(x$reaction), x$odeProduct, sep="*"), ifelse(x$tot == -1, paste("-", paste(as.character(x$reaction),x$odeProduct, sep="*"), sep=""), paste(as.character(x$tot), as.character(x$reaction), x$odeProduct, sep="*")))))
-    
+
     # Now summarize everything per species
     tmpDF2 <- plyr::ddply(tmpDF1, .variable=c("species", "odeSpecies"), function(x) data.frame(res1=paste(x$t1, sep="", collapse=" + "), res2=paste(x$t2, sep="", collapse=" + ")))
-    
+
     # Final tuning - replace + - signs with -, etc.
     tmpDF1 <- plyr::ddply(tmpDF2, .variable=c("species", "odeSpecies"), function(x) data.frame(model=stringr::str_replace_all(as.character(x$res1), "\\+ \\-", "- "), equation=stringr::str_replace_all(as.character(x$res2), "\\+ \\-", "- ")))
-    
+
     # Add model equations to the model
-    y$model <- paste("d_", as.character(tmpDF1$species), " <- ", as.character(tmpDF1$model), sep="")    
-    
+    y$model <- paste("d_", as.character(tmpDF1$species), " <- ", as.character(tmpDF1$model), sep="")
+
     # if rules include any species, include them into the model too  - TBD
-    
+
     # Check if all species are used - TDB
-    
+
     # Create ODEs
     y$odes <- list(equations=paste(paste("r[", 1:nrow(tmpDF1), "]", sep=""), " <- ", as.character(tmpDF1$equation), " - d", as.character(tmpDF1$odeSpecies), sep=""), rates=makeODERates(x), parameters=makeODEParameters(x), rules=makeODERules(x,tmpDF1))
-    
+
     # Mark the model as OK
     y$isChecked <- 1
-    
+
     # Return model to the original object
-    assign(deparse(substitute(x)), y, envir = .GlobalEnv)  
-    
+    assign(deparse(substitute(x)), y, envir = .GlobalEnv)
+
   } else {
     print("No reactions found!")
-  }      
+  }
 }
 
 #makeModel <- cmpfun(makeModel.function)
--- R/validateModel.R
+++ R/validateModel.R
@@ -1,48 +1,50 @@
 #' Print information about the model
-#' 
-#' This function validates model, i.e., checks if all reactions, parameters, rates, rules, etc., are 
+#'
+#' This function validates model, i.e., checks if all reactions, parameters, rates, rules, etc., are
 #' incoorporated into a model, as well as whether all variables have assigned values (either constant or in a form of a function).
-#' 
+#'
 #' @param x a model name
-#' 
+#'
 #' @return Numeric value - 1 if model has been validated, 0 if validation failed
-#' 
+#'
 #' @examples
 #' exmp <- newModel("This is an example of a new model")
 #' addMAreaction(exmp, react="A = null", "rf", "rb")
 #' addMAreaction(exmp, react="A + B -> 2*AB", "k", name="Forward AB")
 #' addMAreaction(exmp, react="AB -> null", "rAB")
-#' 
+#'
 #' addMAreactRate(exmp, "rf", "fixed", "1")
 #' addMAreactRate(exmp, "rb", "fixed", "0.75")
 #' addMAreactRate(exmp, "k", "fixed", "0.5")
 #' addMAreactRate(exmp, "rAB", "assigned", "p1*A")
-#' 
+#'
 #' addParameters(exmp, "p1", 0.75)
-#'  
+#'
 #' addSpecies(exmp, "A", 10)
 #' addSpecies(exmp, "B", 10)
 #' addSpecies(exmp, "AB", 0)
-#' 
+#'
 #' addRule(exmp, "rule B", "ODEs", "B=-0.1*AB")
-#' 
+#'
 #' makeModel(exmp)
-#'   
+#'
 #' validateModel(exmp)
-#' 
+#'
 #' @export
-#' 
+#'
 
 #validateModel.function <- function(x){
 validateModel <- function(x){
-  
-  if (!exists(deparse(substitute(x))))
+
+  if ("list" %in% is(x)) {
+  } else {
     stop("Specified model does not exist!")
-  
+  }
+
   print("Validating model... ")
-  
+
   isValidated <- 1
-  
+
   #######
   # There has to be a better way to do this (so it should be TDB)
   # but right now I am going to do it this way, just to be able to validate model
@@ -53,18 +55,18 @@ validateModel <- function(x){
   # Right now, we don't differentiate between different types of components
   #######
   # If there are rules, get everything from the rules (species, rates, parameters, etc)
-  
+
   numR  <- length(x$rules$rRule)
-  
+
   if (numR  > 0){
-    
+
     # Remove spaces from the rule
-    hlp0 <- sapply(1:numR , function(y) {gsub(" ","", x$rules$rRule[y] , fixed=TRUE) }) 
+    hlp0 <- sapply(1:numR , function(y) {gsub(" ","", x$rules$rRule[y] , fixed=TRUE) })
     hlp1 <- unique(unlist(sapply(1:numR , function(y) {strsplit(hlp0, split="[-+*/=)( ]|[^x][0-9]+|^[0-9]+")})))
     rule.all <- hlp1[(hlp1 != "")]
-    
+
     everythingDefined <- c(x$species$sName, x$reaction$r1, x$reaction$r2, x$species$sName, x$rates$rrName, x$parameters$pName)
-    
+
     if (length(setdiff(rule.all, everythingDefined)) != 0){
       print("Problem with rules - not all species/rates/parameters used in the rules are defined... ")
       print("You need to define: ")
@@ -72,40 +74,40 @@ validateModel <- function(x){
       isValidated <- 0
     }
   }
-  
+
   ##########
-  
-  
+
+
   # Check species definitions
   if ((length(x$reaction) > 0) & (length(x$species) > 0)){
-    
+
     numR <- length(x$reaction$reaction)
-  
-    hlp0 <- sapply(1:numR, function(y) {gsub(" ","", x$reaction$reaction[y] , fixed=TRUE) }) 
+
+    hlp0 <- sapply(1:numR, function(y) {gsub(" ","", x$reaction$reaction[y] , fixed=TRUE) })
     hlp1 <- unlist(sapply(1:numR, function(y) {strsplit(hlp0[y], "\\->")}))
     hlp2 <- unlist(sapply(1:length(hlp1), function(y) {strsplit(hlp1[y], "\\=")}))
     hlp3 <- unique(unlist(sapply(1:length(hlp2), function(y) {strsplit(hlp2[y], "[*+)( ]")})))
-    
+
     # Remove numbers from the results (cases like 5*A would be split as 5 and A)
     # This step may return a warning:
-    # "Warning message: NAs introduced by coercion" 
+    # "Warning message: NAs introduced by coercion"
     hlp2 <- suppressWarnings(hlp3[is.na(as.numeric(hlp3))])
-    
+
     numSp <- setdiff(hlp2, "null") # remove null from the reactions
-    
+
     if (length(setdiff(numSp, x$species$sName)) != 0){
       print("Problem with species definition - not all species used in the reactions are defined... ")
       print("You need to define: ")
       print(setdiff(numSp, x$species$sName))
       isValidated <- 0
     }
-    
+
     if (length(setdiff(x$species$sName, c(numSp, "null"))) != 0){
-      
+
       # If not 0, check if the difference is due to species used in the rules
-      
+
       whatIsMissing <- setdiff(x$species$sName, c(numSp, "null"))
-      
+
       if (length(setdiff(whatIsMissing, rule.all)) != 0){
             print("Problem with species definition - all defined species are not used... ")
             print("Unused species are: ")
@@ -117,18 +119,18 @@ validateModel <- function(x){
     print("Model or species definitions are missing... ")
     isValidated <- 0
   }
-     
+
   # Check rates definitions
   if (length(x$rates) > 0){
-      
+
     toCheckRates.assigned <- which(x$rates$rType == "assigned")
-      
+
     numR <- length(toCheckRates.assigned)
-      
+
     if (numR > 0){
       hlp0 <- x$rates$rVal[toCheckRates.assigned]
-        
-      hlp1 <- sapply(1:numR, function(y) {gsub(" ","", hlp0[y] , fixed=TRUE) }) 
+
+      hlp1 <- sapply(1:numR, function(y) {gsub(" ","", hlp0[y] , fixed=TRUE) })
       hlp2.1 <- unlist(sapply(1:numR, function(y) {strsplit(hlp1[y], "\\+")}))
       hlp2.2 <- unlist(sapply(1:length(hlp2.1), function(y) {strsplit(hlp2.1[y], "\\-")}))
       hlp2.3 <- unlist(sapply(1:length(hlp2.2), function(y) {strsplit(hlp2.2[y], "\\*")}))
@@ -136,15 +138,15 @@ validateModel <- function(x){
       hlp2.5 <- unlist(sapply(1:length(hlp2.4), function(y) {strsplit(hlp2.4[y], "\\%")}))
       hlp2.6 <- unlist(sapply(1:length(hlp2.5), function(y) {strsplit(hlp2.5[y], "\\^")}))
       hlp2.7 <- unique(unlist(sapply(1:length(hlp2.6), function(y) {strsplit(hlp2.6[y], "\\log")})))
-          
+
       remove.nums <- hlp2.7[!sapply(hlp2.7, is.numeric)]
-        
+
       numSp.assigned <- setdiff(remove.nums, x$species$sName)
       # This step may return a warning:
-      # "Warning message: NAs introduced by coercion" 
+      # "Warning message: NAs introduced by coercion"
       hlpR <- suppressWarnings(numSp.assigned[is.na(as.numeric(numSp.assigned))])
       whatsIn <- hlpR[(hlpR != "")]
-        
+
       if (length(setdiff(whatsIn, x$parameters$pName)) != 0){
         print("Problem with rates - not all paramaters assigned to rates are defined... ")
         isValidated <- 0
@@ -155,33 +157,33 @@ validateModel <- function(x){
     isValidated <- 0
   }
 
-  
+
   # Check events
-    
+
   if (length(x$events) > 0){
-    
+
     # Check conditions
-  
+
     numE <- length(x$events$condition)
-      
-    hlp1 <- sapply(1:numE, function(y) {gsub(" ","", x$events$condition[y] , fixed=TRUE) }) 
+
+    hlp1 <- sapply(1:numE, function(y) {gsub(" ","", x$events$condition[y] , fixed=TRUE) })
     hlp2.1 <- unlist(sapply(1:numE, function(y) {strsplit(hlp1[y], "\\>")}))
     hlp2.2 <- unlist(sapply(1:length(hlp2.1), function(y) {strsplit(hlp2.1[y], "\\<")}))
     hlp2.3 <- unlist(sapply(1:length(hlp2.2), function(y) {strsplit(hlp2.2[y], "\\=")}))
     hlp2.4 <- unlist(sapply(1:length(hlp2.3), function(y) {strsplit(hlp2.3[y], "\\&")}))
     hlp2.5 <- unique(unlist(sapply(1:length(hlp2.4), function(y) {strsplit(hlp2.4[y], "\\|")})))
-    
+
     remove.nums <- suppressWarnings(hlp2.5[!sapply(hlp2.5, is.numeric)])
-    
+
     if (length(setdiff(remove.nums, c(x$species$sName, "time"))) != 0){
       print(" Problem with events - not all paramaters within condition are defined... ")
       isValidated <- 0
     }
-    
+
     # Check rules
-    
+
     numE <- length(x$events$rule)
-    
+
     hlp2.1 <- unlist(sapply(1:numR, function(y) {strsplit(hlp1[y], "\\+")}))
     hlp2.2 <- unlist(sapply(1:length(hlp2.1), function(y) {strsplit(hlp2.1[y], "\\-")}))
     hlp2.3 <- unlist(sapply(1:length(hlp2.2), function(y) {strsplit(hlp2.2[y], "\\*")}))
@@ -189,23 +191,23 @@ validateModel <- function(x){
     hlp2.5 <- unlist(sapply(1:length(hlp2.4), function(y) {strsplit(hlp2.4[y], "\\%")}))
     hlp2.6 <- unlist(sapply(1:length(hlp2.5), function(y) {strsplit(hlp2.5[y], "\\^")}))
     hlp2.7 <- unique(unlist(sapply(1:length(hlp2.6), function(y) {strsplit(hlp2.6[y], "\\log")})))
-    
+
     remove.nums <- suppressWarnings(hlp2.7[!sapply(hlp2.7, is.numeric)])
-      
+
     if (length(setdiff(remove.nums, c(x$parameters$pName, x$rates$rrName, x$species$sName))) != 0){
       print(" Problem with events - not all paramaters within condition are defined... ")
       isValidated <- 0
     }
-  }  
-  
-  
+  }
+
+
   #######################################################
-  
+
   if (isValidated == 1)
     print("Model has been validated!")
-  
+
   isValidated
-  
+
 }
 
 #validateModel<- cmpfun(validateModel.function)
